4554cdb9ff4fb44d8c21378c2e4cc11579cd493e
Eamonn O'Brien-Strain <eob@well.com>
2010-05-15 10:46:51 -0700

    First version that successfully gets latidute and longitude

---
+++ Makefile
@@ -0,0 +1,18 @@
+# Copyright (c) 2010 Eamonn O'Brien-Strain, eob@well.com
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which is available at http://www.eclipse.org/legal/epl-v10.html
+
+
+
+run: install
+	cd ..; palm-launch -c org.eamonn.nrby; palm-launch org.eamonn.nrby
+
+install: ../org.eamonn.nrby_0.2.0_all.ipk
+	cd ..; palm-install -r org.eamonn.nrby; palm-install org.eamonn.nrby_0.2.0_all.ipk
+
+../org.eamonn.nrby_0.2.0_all.ipk: lint * */* */*/*
+	cd ..; palm-package nrby
+
+lint:
+	java -classpath build/js.jar org.mozilla.javascript.tools.shell.Main build/jslint.js app/assistants/first-assistant.js
--- app/assistants/first-assistant.js
+++ app/assistants/first-assistant.js
@@ -1,3 +1,12 @@
+/* -*- tab-width:4 -*- */
+
+/*
+ * Copyright (c) 2010 Eamonn O'Brien-Strain, eob@well.com
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
 function FirstAssistant() {
 	/* this is the creator function for your scene assistant object. It will be passed all the 
 	   additional parameters (after the scene name) that were passed to pushScene. The reference
@@ -5,36 +14,38 @@ function FirstAssistant() {
 	   that needs the scene controller should be done in the setup function below. */
 }
 
-FirstAssistant.prototype.setup = function() {
+FirstAssistant.prototype.setup = function () {
 	/* this function is for setup tasks that have to happen when the scene is first created */
 		
 	/* use Mojo.View.render to render view templates and add them to the scene, if needed */
 	
 	/* setup widgets here */
-  var result = this.controller.serviceRequest('palm://com.palm.location', {
-    method : 'getCurrentPosition',
-	parameters: { responseTime: 2, subscribe: false }/*,
-	onSuccess: this.handleServiceResponse.bind(this),
-	onFailure: this.handleServiceResponseError.bind(this)*/
+    var ctl = this.controller;
+    ctl.serviceRequest('palm://com.palm.location', {
+	    method : 'getCurrentPosition',
+		parameters: { responseTime: 2, subscribe: false },
+		onSuccess: function (response) {
+			ctl.get("latlon").update(response.latitude + "," + response.longitude);
+		},
+	    onFailure: function (response) {
+		    ctl.get("latlon").update("Error getting GPS info: " + response);
+		}
 	});
-  console.log("getCurrentPosition returned "+result.toString());
-  var latlon = ( result.errorCode===0 ) ? (result.latitude+","+result.longitude) : ("GPS error "+result.errorCode);
 
-  this.controller.get("latlon").update( latlon );
 	/* add event handlers to listen to events from widgets */
 };
 
-FirstAssistant.prototype.activate = function(event) {
+FirstAssistant.prototype.activate = function (event) {
 	/* put in event handlers here that should only be in effect when this scene is active. For
 	   example, key handlers that are observing the document */
 };
 
-FirstAssistant.prototype.deactivate = function(event) {
+FirstAssistant.prototype.deactivate = function (event) {
 	/* remove any event handlers you added in activate and do any other cleanup that should happen before
 	   this scene is popped or another scene is pushed on top */
 };
 
-FirstAssistant.prototype.cleanup = function(event) {
+FirstAssistant.prototype.cleanup = function (event) {
 	/* this function should do any cleanup needed before the scene is destroyed as 
 	   a result of being popped off the scene stack */
 };
