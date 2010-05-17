3e58e18329cc0478340b1ff1aa24e49687c4cfc5
Eamonn O'Brien-Strain <eob@well.com>
2010-05-17 11:45:27 -0700

    got scrolling working

--- app/assistants/first-assistant.js
+++ app/assistants/first-assistant.js
@@ -22,11 +22,34 @@ function FirstAssistant() {
 
 /* this function is for setup tasks that have to happen when the scene is first created */
 FirstAssistant.prototype.setup = function () {
-    var assistant, ctl, placeName, photos, photoIndex, prevTime;
+    var assistant, viewer, ctl, placeName, photos, photoIndex, prevTime;
 
 
 	assistant = this; //for use in lambda functions
     ctl = this.controller;
+	viewer = $('ImageId');
+
+	function photoUrlBase(photo) {
+	    return 'http://farm' + photo.farm +
+			  '.static.flickr.com/' + photo.server + 
+			  '/' +  photo.id +
+			  '_' +  photo.secret;
+	}
+
+	function urlBaseLeft() {
+	    console.log("urlBaseLeft  photoIndex=" + photoIndex + " photos.length=" + photos.length);
+	    return photoUrlBase(photos[(photoIndex - 1 + photos.length) % photos.length]);
+	}
+
+	function urlBaseCenter() {
+	    console.log("urlBaseCenter  photoIndex=" + photoIndex + " photos.length=" + photos.length);
+	    return photoUrlBase(photos[photoIndex]);
+	}
+
+	function urlBaseRight() {
+	    console.log("urlBaseRight  photoIndex=" + photoIndex + " photos.length=" + photos.length);
+	    return photoUrlBase(photos[(photoIndex + 1) % photos.length]);
+	}
 
 	this.attributes = {
 		//noExtractFS : true	//optional, turn off using extractfs to speed up renders.
@@ -34,21 +57,24 @@ FirstAssistant.prototype.setup = function () {
 	this.model = {
 		background: 'black',  
 		onLeftFunction : function (event) {
+		    photoIndex = (photoIndex - 1 + photos.length) % photos.length;
+		    viewer.mojo.leftUrlProvided(urlBaseLeft() + '_d.jpg', urlBaseLeft() + '_m_d.jpg');
 	    }.bind(this),
 		onRightFunction : function (event) {
+		    photoIndex = (photoIndex + 1) % photos.length;
+		    viewer.mojo.rightUrlProvided(urlBaseRight() + '_d.jpg', urlBaseRight() + '_m_d.jpg');
 	    }.bind(this)
 	};
 
 	this.controller.setupWidget('ImageId', this.attributes, this.model);
 
-	this.myPhotoDivElement = $('ImageId');
 
 	Mojo.Event.listen(
 	    this.controller.get('ImageId'), 
 		Mojo.Event.imageViewChanged, 
 		function (event) {
 		    /* Do something when the image view changes */
-		    assistant.myPhotoDivElement.mojo.manualSize(Mojo.Environment.DeviceInfo.screenWidth, Mojo.Environment.DeviceInfo.screenHeight);
+		    viewer.mojo.manualSize(Mojo.Environment.DeviceInfo.screenWidth, Mojo.Environment.DeviceInfo.screenHeight);
 		}.bindAsEventListener(this)
 	);
 
@@ -79,29 +105,15 @@ FirstAssistant.prototype.setup = function () {
 		return d.getTime();
 	}
 
-	function photoUrlBase(photo) {
-	    return 'http://farm' + photo.farm +
-			  '.static.flickr.com/' + photo.server + 
-			  '/' +  photo.id +
-			  '_' +  photo.secret;
-	}
-
 		
 	function showPhoto() {
-	    var photo, urlBase;
+		console.log("showPhoto() photoIndex=" + photoIndex);
 		if (photoIndex >= 0 && photos.length > 0) {
-			photo = photos[photoIndex];
-			photoIndex = (photoIndex + 1) % photos.length;
-			console.log("title=" + photo.title);
-			urlBase = photoUrlBase(photo);
-			console.log("PHOTO URL BASE " + urlBase);
-			//ctl.get("nrby-photo").src = url;
-			assistant.myPhotoDivElement.mojo.leftUrlProvided(urlBase + '_d.jpg', urlBase + '_m_d.jpg');
-			assistant.myPhotoDivElement.mojo.centerUrlProvided(urlBase + '_d.jpg', urlBase + '_m_d.jpg');
-			assistant.myPhotoDivElement.mojo.rightUrlProvided(urlBase + '_d.jpg', urlBase + '_m_d.jpg');
-			
-			//ctl.get("nrby-title").update(photo.title);
+		    viewer.mojo.leftUrlProvided(urlBaseLeft() + '_d.jpg', urlBaseLeft() + '_m_d.jpg');
+		    viewer.mojo.centerUrlProvided(urlBaseCenter() + '_d.jpg', urlBaseCenter() + '_m_d.jpg');
+		    viewer.mojo.rightUrlProvided(urlBaseRight() + '_d.jpg', urlBaseRight() + '_m_d.jpg');
 		}
+		console.log("FINISHED showPhoto()");
 	}
 
     ctl.serviceRequest('palm://com.palm.location', {
@@ -110,7 +122,7 @@ FirstAssistant.prototype.setup = function () {
 		onSuccess: function (response) {
 
 		    /* throttle the calls to Flickr */
-		    if ((now() - prevTime) < 10000) {
+		    if ((now() - prevTime) < 60000) {
 			    return;  /* too soon */
 			}
 			prevTime = now();
@@ -134,7 +146,7 @@ FirstAssistant.prototype.setup = function () {
 						place = places[0]; 
 
 						if (place.name === placeName) {
-						    showPhoto();
+						    //showPhoto();
 						    return;
 						}
 						placeName = place.name;
@@ -145,16 +157,33 @@ FirstAssistant.prototype.setup = function () {
 						    console.log("PHOTO SEARCH returns " + transport.responseText);
 							response = Mojo.parseJSON(transport.responseText);
 							if (response.stat !== "ok") {
+							    console.log("Error from flickr.photo.search transport.resonseText");
 								assistant.showDialogBox("Error from Fickr", transport.responseText);
 							} else {
-							    photos = response.photos.photo;
-								photoIndex = 0;
+							    n = response.photos.photo.length;
+
+							    console.log("photo search returned " + n + " photos");
+
+								// Arrange photos ...,9,7,5,3,1,0,2,4,6,8,...
+								// so that most interesting are
+								// closest to center
+								for (i = 0; i < n; i += 1) {
+								    if (i % 2 === 0) { //even
+									    photos[n / 2  +  i / 2] = response.photos.photo[i];
+										console.log(i + " --> " + (n / 2  +  i / 2));
+								    } else { //odd
+									    photos[n / 2  -  (i + 1) / 2] = response.photos.photo[i];
+										console.log(i + " --> " + (n / 2  -  (i + 1) / 2));
+									}
+								}
+								photoIndex = n / 2;
 								showPhoto();
 							}
 						};
+						console.log("about to call flickr.photos.search ...");
 						callFlickr(
 						    'photos.search',
-							'sort=interestingness-desc&place_id=' + place.place_id,
+							'per_page=10&sort=interestingness-desc&place_id=' + place.place_id,
 							flickrSearchHandler
 							);
 					}
@@ -184,7 +213,7 @@ FirstAssistant.prototype.deactivate = function (event) {
 FirstAssistant.prototype.cleanup = function (event) {
 	/* this function should do any cleanup needed before the scene is destroyed as 
 	   a result of being popped off the scene stack */
-	Mojo.Event.stopListening(this.controller.get('ImageId'), Mojo.Event.imageViewChanged, this.imageViewChanged);
+	//Mojo.Event.stopListening(this.controller.get('ImageId'), Mojo.Event.imageViewChanged, this.imageViewChanged);
 };
 
 // This function will popup a dialog, displaying the message passed in.
