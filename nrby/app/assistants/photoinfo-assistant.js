90d5b2a8c1b45305988af6ca782d0f8e8ee2ac77
Eamonn O'Brien-Strain <eob@well.com>
2010-10-29 15:47:56 -0700

    Add tumbnail on photo info page and tweak appearance of title so it looks better. [finish 5957374]

---
+++ nrby/app/assistants/photoinfo-assistant.js
@@ -0,0 +1,50 @@
+/* -*- tab-width:4 -*- */
+
+/*
+ * Copyright (c) 2010 Eamonn O'Brien-Strain, eob@well.com
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+/*jslint devel: true */
+
+/* declare globals to keep JSLint happy */
+var $; //framework
+
+/** @class The controller for the scene that shows information and
+provides controls for a particular photo. */
+function PhotoinfoAssistant(photo) {
+	console.log("PhotoInfoAssistant(" + photo.title + ")");
+	$('infoTitle').update(photo.title);
+	$('infoThumb').setAttribute('src', photo.url_t);
+	/* this is the creator function for your scene assistant object. It will be passed all the 
+	   additional parameters (after the scene name) that were passed to pushScene. The reference
+	   to the scene controller (this.controller) has not be established yet, so any initialization
+	   that needs the scene controller should be done in the setup function below. */
+}
+
+PhotoinfoAssistant.prototype.setup = function () {
+	/* this function is for setup tasks that have to happen when the scene is first created */
+		
+	/* use Mojo.View.render to render view templates and add them to the scene, if needed */
+	
+	/* setup widgets here */
+	
+	/* add event handlers to listen to events from widgets */
+};
+
+PhotoinfoAssistant.prototype.activate = function (event) {
+	/* put in event handlers here that should only be in effect when this scene is active. For
+	   example, key handlers that are observing the document */
+};
+
+PhotoinfoAssistant.prototype.deactivate = function (event) {
+	/* remove any event handlers you added in activate and do any other cleanup that should happen before
+	   this scene is popped or another scene is pushed on top */
+};
+
+PhotoinfoAssistant.prototype.cleanup = function (event) {
+	/* this function should do any cleanup needed before the scene is destroyed as 
+	   a result of being popped off the scene stack */
+};
