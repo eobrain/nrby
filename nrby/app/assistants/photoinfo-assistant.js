6abb5478c6f303c47bfcdf4007111800d5ca8aef
Eamonn O'Brien-Strain <eob@well.com>
2010-10-29 17:00:52 -0700

    Add button to view FLickr page of current photo

--- nrby/app/assistants/photoinfo-assistant.js
+++ nrby/app/assistants/photoinfo-assistant.js
@@ -10,28 +10,50 @@
 /*jslint devel: true */
 
 /* declare globals to keep JSLint happy */
-var $; //framework
+var Mojo, $; //framework
 
 /** @class The controller for the scene that shows information and
 provides controls for a particular photo. */
 function PhotoinfoAssistant(photo) {
-	console.log("PhotoInfoAssistant(" + photo.title + ")");
-	$('infoTitle').update(photo.title);
-	$('infoThumb').setAttribute('src', photo.url_t);
 	/* this is the creator function for your scene assistant object. It will be passed all the 
 	   additional parameters (after the scene name) that were passed to pushScene. The reference
 	   to the scene controller (this.controller) has not be established yet, so any initialization
 	   that needs the scene controller should be done in the setup function below. */
+	console.log("PhotoInfoAssistant(" + photo.title + ")");
+	this.photo = photo;
+	$('infoTitle').update(photo.title);
+	$('infoThumb').setAttribute('src', photo.url_t);
 }
 
 PhotoinfoAssistant.prototype.setup = function () {
+	var self, gotoPhotoPage;
+
+	self = this;
+
 	/* this function is for setup tasks that have to happen when the scene is first created */
 		
 	/* use Mojo.View.render to render view templates and add them to the scene, if needed */
 	
 	/* setup widgets here */
+	this.controller.setupWidget('gotoPhotoPage', {}, {label : "View on Flickr"});
+
+	gotoPhotoPage = function (event) {
+		console.log("GOTO PHOTO PAGE BUTTON PRESSED");
+		self.controller.serviceRequest("palm://com.palm.applicationManager", {
+			method: "open",
+			parameters:  {
+				id: 'com.palm.app.browser',
+				params: {
+					target: self.photo.photoPage()
+				}
+			}
+		}); 
+		
+	}.bindAsEventListener(this);	
+	
 	
 	/* add event handlers to listen to events from widgets */
+	Mojo.Event.listen($('gotoPhotoPage'), Mojo.Event.tap, gotoPhotoPage);
 };
 
 PhotoinfoAssistant.prototype.activate = function (event) {
